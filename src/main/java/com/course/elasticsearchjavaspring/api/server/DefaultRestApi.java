071efc1bb26d34b27c6b74b3138a6199e671bb31
Jeff Hsu <toy7755679@gmail.com>
2020-08-28 21:54:12 +0800

    Add log to DefaultRestApi

--- src/main/java/com/course/elasticsearchjavaspring/api/server/DefaultRestApi.java
+++ src/main/java/com/course/elasticsearchjavaspring/api/server/DefaultRestApi.java
@@ -1,6 +1,10 @@
 package com.course.elasticsearchjavaspring.api.server;
 
+import java.time.LocalTime;
+
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class DefaultRestApi {
 
+	private Logger LOG = LoggerFactory.getLogger(DefaultRestApi.class);
+
 	@RequestMapping(value = "/welcome")
 	public String welcome() {
-		System.out.println(StringUtils.join("Spring", "Success"));
+		LOG.info(StringUtils.join("Spring", "Success"));
 		return "Hello, Everybodyyyyyy!";
+	}
+
+	@RequestMapping(value = "/time")
+	public String time() {
+		return LocalTime.now().toString();
 	}
 }
